475940e4a0440683c5cd2a46faf939a8b9dfef60
Henrique D. Garcia <henriquedgarcia@gmail.com>
2019-07-08 11:03:31 -0300

    Função plot1.

--- main.py
+++ main.py
@@ -53,6 +53,9 @@ def plot2():
 def plot1():
     for file in glob.glob('head_movement/*.csv'):
         df = pd.read_csv(file)
+        folder = 'results/plot1'
+        os.makedirs(folder, exist_ok=True)
+
         name = os.path.basename(file)
         name = os.path.splitext(name)[0]
 
@@ -65,6 +68,8 @@ def plot1():
         #    'filteredYaw', 'filteredPitch', 'filteredRoll',
         #    'mappedTX', 'mappedTY', 'mappedTZ',
         #    'mappedYaw', 'mappedPitch', 'mappedRoll']
+
+        # Plotando amostras Raw
         plt.close()
         fig, ax = plt.subplots(3, 1, sharex='all', dpi=100, figsize=(6, 6))
         ax[0].plot(df['rawYaw'])
@@ -74,10 +79,11 @@ def plot1():
         ax[0].set_title('raw measures')
         ax[2].set_xlabel('samples')
 
-        name_raw = f'head_movement/{name}_raw'
+        name_raw = f'{folder}/{name}_raw'
         fig.savefig(name_raw, transparent=True)
         # plt.show()
 
+        # Plotando amostras Corrected
         plt.close()
         fig, ax = plt.subplots(3, 1, sharex='all', dpi=100, figsize=(6, 6))
         ax[0].plot(df['correctedYaw'])
@@ -87,11 +93,25 @@ def plot1():
         ax[0].set_title('corrected measures')
         ax[2].set_xlabel('samples')
 
-        name_corrected = f'head_movement/{name}_corr'
+        name_corrected = f'{folder}/{name}_corr'
         fig.savefig(name_corrected, transparent=True)
         # plt.show()
 
+        # Plotando amostras mapped
+        plt.close()
+        fig, ax = plt.subplots(3, 1, sharex='all', dpi=100, figsize=(6, 6))
+        ax[0].plot(df['mappedYaw'])
+        ax[1].plot(df['mappedPitch'])
+        ax[2].plot(df['mappedRoll'])
+
+        ax[0].set_title('mapped measures')
+        ax[2].set_xlabel('samples')
+
+        name_mapped = f'{folder}/{name}_mapped'
+        fig.savefig(name_mapped, transparent=True)
+        # plt.show()
         print('')
 
 
 if __name__ == '__main__':
+    # plot1()
